ea746f7cefcc6a3fcd56310fd7031f30d3d823af
Nakita Sun <nakita.sun@everbridge.com>
2019-06-24 10:39:06 +0800

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Router, Route, Link } from "react-router";
 
 function App() {
   return (
