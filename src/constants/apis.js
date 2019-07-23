582d2d1665e6530e1c58d1065812287d41a89248
Nakita Sun <k626541201@hotmail.com>
2019-07-23 17:53:23 +0800

    Add mock data, get positions using axios

---
+++ src/constants/apis.js
@@ -0,0 +1,2 @@
+// export const PLACE_SEARCH_API = 'https://h5.ele.me/restapi/bgs/poi/search_poi_nearby_alipay?keyword=yin&offset=0&limit=20';
+export const PLACE_SEARCH_API = 'mock/locations.json';
--- src/page/Home/components/GeoLocation/index.js
+++ src/page/Home/components/GeoLocation/index.js
@@ -1,18 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { PLACE_SEARCH_API } from 'constants/apis';
 
 const GeoLocation = () => {
   const [latLong, setLatLong] = useState({});
+  const [positionStr, setPositionStr] = useState('');
   const getCurrentLatLong = () => {
     navigator.geolocation.getCurrentPosition((location) => {
       setLatLong({ lat: location.coords.latitude, lng: location.coords.longitude });
     });
   };
+  async function getPosition() {
+    await axios
+      .get(PLACE_SEARCH_API, {
+        latitude: latLong.latitude,
+        longitude: latLong.longitude,
+      })
+      .then((res) => {
+        setPositionStr(res);
+      });
+  }
   useEffect(() => {
     getCurrentLatLong();
+    getPosition();
   }, []);
   return (
     <div>
       <h2>{JSON.stringify(latLong)}</h2>
+      <h2>{JSON.stringify(positionStr)}</h2>
       <div className="gap short" style={{ background: 'linear-gradient(#fff, #ddd)' }} />
     </div>
   );
