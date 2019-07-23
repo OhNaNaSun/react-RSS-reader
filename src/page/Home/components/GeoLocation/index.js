5b2fb5652c850500069c84e2d6f4f5ab3d3fc9b2
Nakita Sun <k626541201@hotmail.com>
2019-07-23 15:23:50 +0800

    Home page: get current latLong for header using react hook

--- src/page/Home/components/GeoLocation/index.js
+++ src/page/Home/components/GeoLocation/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const GeoLocation = () => (
-  <div>
-    <h2>Content before the Sticky...</h2>
-    <div className="gap short" style={{ background: 'linear-gradient(#fff, #ddd)' }} />
-  </div>
-);
+const GeoLocation = () => {
+  const [latLong, setLatLong] = useState({});
+  const getCurrentLatLong = () => {
+    navigator.geolocation.getCurrentPosition((location) => {
+      setLatLong({ lat: location.coords.latitude, lng: location.coords.longitude });
+    });
+  };
+  useEffect(() => {
+    getCurrentLatLong();
+  }, []);
+  return (
+    <div>
+      <h2>{JSON.stringify(latLong)}</h2>
+      <div className="gap short" style={{ background: 'linear-gradient(#fff, #ddd)' }} />
+    </div>
+  );
+};
 
 export default GeoLocation;
