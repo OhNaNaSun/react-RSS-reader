3f5ef5d218c24a074abcef679add4ff40c101d14
Nakita Sun <k626541201@hotmail.com>
2019-08-14 17:51:35 +0800

    Add header for home page, replace it with habit

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import { StickyContainer, Sticky } from 'react-sticky';
-import GeoLocation from './components/GeoLocation';
-import SearchBox from './components/SearchBox';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 
-const Home = () => (
-  <div>
-    <GeoLocation />
-    <StickyContainer>
-      <Sticky>
-        {({ style }) => (
-          <header style={style}>
-            {' '}
-            <SearchBox />
-          </header>
-        )}
-      </Sticky>
-      <div style={{ height: '700px' }} />
-    </StickyContainer>
-  </div>
-);
+const useStyles = makeStyles({
+    gridItem: {
+        border: "1px black solid"
+    }
+});
 
-export default Home;
+export default function ButtonAppBar() {
+    const itemArr = [1, 2, 3, 4, 5, 6, 7];
+    const classes = useStyles();
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Grid container spacing={0}>
+                        {itemArr.map(() => (
+                            <Grid className={classes.gridItem} item xs>
+                                xs
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
