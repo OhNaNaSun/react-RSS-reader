f0fa9fb45c353f0bbca4adfc7436402c7c9ed880
Nakita Sun <k626541201@hotmail.com>
2019-08-22 17:14:32 +0800

    Add one feed content to page

--- src/page/App/feedContainer.tsx
+++ src/page/App/feedContainer.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, FunctionComponent } from 'react';
-
 const Parser = require('rss-parser');
+
 const parser = new Parser({
     customFields: {
         item: ['media:description', 'description'],
     },
 });
 const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-export const FeedContainer: FunctionComponent = () => {
+const FeedContainer: FunctionComponent = () => {
     const [data, dataSet] = useState([{ title: '', description: '' }]);
     const fetchMyAPI = async (): Promise<number> => {
         const feed = await parser.parseURL(CORS_PROXY + 'https://ponyfoo.com/articles/feed');
@@ -21,8 +21,9 @@ export const FeedContainer: FunctionComponent = () => {
     return (
         <div>
             {data.slice(0, 1).map(item => (
-                <div key={item.title}>{item.description}</div>
+                <div key={item.title} dangerouslySetInnerHTML={{ __html: item.description }}></div>
             ))}
         </div>
     );
 };
+export default FeedContainer;
