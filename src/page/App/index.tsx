73cfcdb88fa7705679b6bc6f63b469981d68a1ad
Nakita Sun <k626541201@hotmail.com>
2019-08-23 10:18:06 +0800

    Add 'Add feed' button in UI

--- src/page/App/index.tsx
+++ src/page/App/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import MiniDrawer from './MiniDrawer';
 import NestedList from './NestedList';
 import Divider from '@material-ui/core/Divider';
-import './FeedContainer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AddIcon from '@material-ui/icons/Add';
 
 const App: React.SFC = () => {
     return (
@@ -16,6 +20,14 @@ const App: React.SFC = () => {
                 <Divider />
                 <NestedList />
                 <NestedList />
+                <List>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <AddIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Add" />
+                    </ListItem>
+                </List>
             </MiniDrawer>
         </div>
     );
