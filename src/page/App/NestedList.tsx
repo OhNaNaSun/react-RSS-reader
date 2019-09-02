70f4a67ad1ddb6c2e8ff636a4a96fdcaa7ccccfd
S-iscoming <626541201@qq.com>
2019-09-02 22:19:44 +0800

    Commit with some style errors, kill me

--- src/page/App/NestedList.tsx
+++ src/page/App/NestedList.tsx
@@ -9,10 +9,11 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import StarBorder from '@material-ui/icons/StarBorder';
+import blue from '@material-ui/core/colors/blue';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {
+        root:{
             width: '100%',
             maxWidth: 360,
             backgroundColor: theme.palette.background.paper,
@@ -20,18 +21,21 @@ const useStyles = makeStyles((theme: Theme) =>
         nested: {
             paddingLeft: theme.spacing(4),
         },
+        currented: {
+            color: theme.palette.primary
+        },
     }),
 );
 interface FeedListProps {
     feedType: string;
     feeds: string[];
-
     handleClickFeed: (feedUrl: string) => void;
 }
 const NestedList: React.SFC<FeedListProps> = props => {
     const classes = useStyles();
     const { feedType, feeds, handleClickFeed } = props;
     const [open, setOpen] = React.useState(false);
+    const [currentFeed, setCurrentFeed] = React.useState('');
     function handleClick(): void {
         setOpen(!open);
     }
@@ -48,6 +52,8 @@ const NestedList: React.SFC<FeedListProps> = props => {
             <Collapse in={open} timeout="auto" unmountOnExit>
                 <List disablePadding>
                     {feeds.map((feed: string) => {
+                        const currentColor =  feed === currentFeed ? 'primary':'inherit';
+                        // console.log('className', classNames)
                         return (
                             <ListItem
                                 key={feed}
@@ -56,13 +62,14 @@ const NestedList: React.SFC<FeedListProps> = props => {
                                 className={classes.nested}
                                 onClick={(): void => {
                                     console.log('feedUrl', feed);
+                                    setCurrentFeed(feed)
                                     handleClickFeed(feed);
                                 }}
                             >
                                 <ListItemIcon>
-                                    <StarBorder />
+                                    <StarBorder color={currentColor} />
                                 </ListItemIcon>
-                                <ListItemText primary={feed} />
+                                <ListItemText className={classes.currented} primary={feed} />
                             </ListItem>
                         );
                     })}
