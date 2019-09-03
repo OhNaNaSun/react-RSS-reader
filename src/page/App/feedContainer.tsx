6bf2c3fc7be7b9624b8920c5b0a91b5796320451
Nakita Sun <k626541201@hotmail.com>
2019-09-03 13:27:11 +0800

    Remove useless code to test github

--- src/page/App/feedContainer.tsx
+++ src/page/App/feedContainer.tsx
@@ -1,12 +1,10 @@
 import { CORS_PROXY } from './constants';
-import React, { useState, useEffect, FunctionComponent, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
