d719525c9e249746a2edd1c714cdcdf79c72a122
Nakita Sun <k626541201@hotmail.com>
2019-09-03 10:48:36 +0800

    Revert "Commit with some style errors, kill me"
    This reverts commit 70f4a67ad1ddb6c2e8ff636a4a96fdcaa7ccccfd.

--- src/page/App/NestedList.tsx
+++ src/page/App/NestedList.tsx
@@ -9,11 +9,10 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import StarBorder from '@material-ui/icons/StarBorder';
-import blue from '@material-ui/core/colors/blue';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root:{
+        root: {
             width: '100%',
             maxWidth: 360,
             backgroundColor: theme.palette.background.paper,
@@ -21,21 +20,18 @@ const useStyles = makeStyles((theme: Theme) =>
         nested: {
             paddingLeft: theme.spacing(4),
         },
-        currented: {
-            color: theme.palette.primary
-        },
     }),
 );
 interface FeedListProps {
     feedType: string;
     feeds: string[];
+
     handleClickFeed: (feedUrl: string) => void;
 }
 const NestedList: React.SFC<FeedListProps> = props => {
     const classes = useStyles();
     const { feedType, feeds, handleClickFeed } = props;
     const [open, setOpen] = React.useState(false);
-    const [currentFeed, setCurrentFeed] = React.useState('');
     function handleClick(): void {
         setOpen(!open);
     }
@@ -52,8 +48,6 @@ const NestedList: React.SFC<FeedListProps> = props => {
             <Collapse in={open} timeout="auto" unmountOnExit>
                 <List disablePadding>
                     {feeds.map((feed: string) => {
-                        const currentColor =  feed === currentFeed ? 'primary':'inherit';
-                        // console.log('className', classNames)
                         return (
                             <ListItem
                                 key={feed}
@@ -62,14 +56,13 @@ const NestedList: React.SFC<FeedListProps> = props => {
                                 className={classes.nested}
                                 onClick={(): void => {
                                     console.log('feedUrl', feed);
-                                    setCurrentFeed(feed)
                                     handleClickFeed(feed);
                                 }}
                             >
                                 <ListItemIcon>
-                                    <StarBorder color={currentColor} />
+                                    <StarBorder />
                                 </ListItemIcon>
-                                <ListItemText className={classes.currented} primary={feed} />
+                                <ListItemText primary={feed} />
                             </ListItem>
                         );
                     })}
