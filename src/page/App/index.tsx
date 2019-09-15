d501ac64336d147955ff5c03f24722b489e2e835
S-iscoming <626541201@qq.com>
2019-09-15 14:12:28 +0800

    Add fontfamily to app

--- src/page/App/index.tsx
+++ src/page/App/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import MiniDrawer from './MiniDrawer';
-
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+const theme = createMuiTheme({
+    typography: {
+        // Tell Material-UI what the font-size on the html element is.
+        // htmlFontSize: 10,
+        fontFamily: ['Merriweather', 'sans-serif'].join(','),
+    },
+});
 const App: React.SFC = () => {
     return (
-        <div>
+        <ThemeProvider theme={theme}>
             <MiniDrawer></MiniDrawer>
-        </div>
+        </ThemeProvider>
     );
 };
 export default App;
